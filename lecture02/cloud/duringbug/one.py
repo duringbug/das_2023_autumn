2f2674cfc785ab7b15863464743aba5fd82accd8
duringbug <2034298621@qq.com>
2023-09-25 13:20:00 +0800

    lec2 pro1

--- lecture02/cloud/duringbug/one.py
+++ lecture02/cloud/duringbug/one.py
@@ -3,28 +3,36 @@ Description:
 Author: 唐健峰
 Date: 2023-09-19 18:13:32
 LastEditors: ${author}
-LastEditTime: 2023-09-25 12:31:05
+LastEditTime: 2023-09-25 13:17:15
 '''
 import math
 
-store = {}
-son = []
 
+class MaxFinder:
+    def __init__(self):
+        self.store = {}
+        self.son = {}
 
-def find_max(num):
-    global store
-    global son
-    if num in store:
-        return store[num]
-    else:
-        max = num
-        num_1 = 0
-        num_2 = num
-        for i in range(1, math.floor(num/2)+1):
-            r = find_max(i)*find_max(num-i)
-            if r > max:
-                max = r
-                num_1 = i
-                num_2 = num-i
-        store[num] = max
-        return max
+    def find_max(self, num):
+        if num in self.store:
+            return self.store[num]
+        else:
+            max_value = num
+            num_1 = 0
+            num_2 = num
+            for i in range(1, math.floor(num/2)+1):
+                r = self.find_max(i) * self.find_max(num-i)
+                if r > max_value:
+                    max_value = r
+                    num_1 = i
+                    num_2 = num-i
+            self.store[num] = max_value
+            self.son[num] = [num_1, num_2]
+            return max_value
+
+    def print_result(self, num):
+        if num <= 3:
+            print(num, end=" ")
+        else:
+            self.print_result(self.son[num][0])
+            self.print_result(self.son[num][1])
--- lecture02/index.py
+++ lecture02/index.py
@@ -3,17 +3,26 @@ Description:
 Author: 唐健峰
 Date: 2023-09-19 18:12:37
 LastEditors: ${author}
-LastEditTime: 2023-09-25 12:29:18
+LastEditTime: 2023-09-25 13:18:53
 '''
-from cloud.duringbug.one import find_max
+from cloud.duringbug.one import *
 import sys
 
 sys.setrecursionlimit(10000)
 
 
 def main():
-    # 不包括1，2，3，因为1，2，3本身就不用分了
-    print(find_max(4))
+    try:
+        num = 2001
+        # 不包括1，2，3，因为1，2，3本身就不用分了
+        max_finder = MaxFinder()
+        result = max_finder.find_max(num)
+        # print("Result:", result)
+        # print("Store:", max_finder.store)
+        # print("Son:", max_finder.son)
+        max_finder.print_result(num)
+    except:
+        print("error input!!!")
     return 0
 
 

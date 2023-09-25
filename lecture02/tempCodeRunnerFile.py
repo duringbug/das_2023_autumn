c31b18069daf50c5cbc387ac2ed4a40bcedca3f6
duringbug <2034298621@qq.com>
2023-09-25 13:22:32 +0800

    lec2 pro1

--- lecture02/tempCodeRunnerFile.py
+++ lecture02/tempCodeRunnerFile.py
@@ -1,2 +1,8 @@
-
-    print(FindMax(2001).find_max(2))
+'''
+Description: 
+Author: 唐健峰
+Date: 2023-09-19 19:15:08
+LastEditors: ${author}
+LastEditTime: 2023-09-25 13:21:54
+'''
+print(FindMax(2001).find_max(2))
